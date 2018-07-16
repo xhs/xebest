8730692aa765092068387b92d27fa7fcbe43f8e7
xiaohson <xiaohson@XIAOHSON-CN.al.oracle.com>
2018-07-16 14:42:10 +0800

    finish status page

--- src/js/viewModels/status.js
+++ src/js/viewModels/status.js
@@ -10,9 +10,31 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'config'],
         self.router.go('home')
       }
 
+      self.nodesRunning = ko.observable('-')
+      self.nodesStopped = ko.observable('-')
+      self.healthPercent = ko.observable('-')
+      self.blocks = ko.observable('-')
+      self.transactions = ko.observable('-')
+
       self.connected = function () {
+        self.nodesRunning('-')
+        self.nodesStopped('-')
+        self.healthPercent('-')
+        self.blocks('-')
+        self.transactions('-')
 
-
+        $.ajax({
+          url: config.getBaseUrl() + '/coldChainLogistics/getBlockChainInfo.do'
+        }).done(function (data) {
+          var response = JSON.parse(data)
+          console.log(response)
+          var result = response.resultMsg
+          self.nodesRunning(result.nodesRunning)
+          self.nodesStopped(result.nodesStopped)
+          self.healthPercent(result.healthStatus + '%')
+          self.blocks(result.blocks)
+          self.transactions(result.transactions)
+        })
       }
 
       self.disconnected = function () {
